7ef9ab2edfec6da22dd625c90868cefa358b7d5d
render <142716427+render217@users.noreply.github.com>
2025-06-13 00:59:34 +0300

    feat: refine feature card with data and hover

--- src/app/(landing)/_components/features/index.tsx
+++ src/app/(landing)/_components/features/index.tsx
@@ -5,49 +5,137 @@ import { cn } from '@/lib/utils';
 import { ClassValue } from 'clsx';
 import Image from 'next/image';
 
+interface IDetailData {
+  title: string;
+  text: string;
+}
 interface IFeature {
   img: string;
   title: string;
   description: string;
+  detailData: IDetailData[];
 }
 
 const featuresData = [
-  {
-    img: '/assets/svg/work-flow-icon.svg',
-    title: 'Create A Work Flow',
-    description:
-      'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
-  },
-  {
-    img: '/assets/svg/ats-icon.svg',
-    title: 'Integration With CRM/ATS',
-    description:
-      'Seamlessly connect with your existing CRM or Applicant Tracking System (ATS) to streamline candidate management and enhance data flow across platforms.',
-  },
-  {
-    img: '/assets/svg/ai-agents-icon.svg',
-    title: 'Industry-specific AI agents',
-    description:
-      'Leverage tailored AI agents designed for specific industries, providing targeted insights and recommendations to optimize your recruitment process.',
-  },
-  //
-  {
-    img: '/assets/svg/resume-screening-icon.svg',
-    title: 'Resume screening',
-    description:
-      'Utilize AI-driven resume screening to quickly identify top candidates by analyzing qualifications, experience, and skills against job requirements.',
-  },
   {
     img: '/assets/svg/phone-sc-icon.svg',
     title: 'AI-Powered Phone Screening',
     description:
       'Automate initial candidate evaluations with AI-powered phone screenings, delivering consistent assessments and saving time in the hiring process.',
+    detailData: [
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+    ],
   },
   {
     img: '/assets/svg/prompts-icon.svg',
     title: 'Custom Prompts',
     description:
       'Create tailored prompts for interviews and assessments, enabling focused evaluations that align with your specific hiring criteria and company culture.',
+    detailData: [
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+    ],
+  },
+  {
+    img: '/assets/svg/ai-agents-icon.svg',
+    title: 'Industry-specific AI agents',
+    description:
+      'Leverage tailored AI agents designed for specific industries, providing targeted insights and recommendations to optimize your recruitment process.',
+    detailData: [
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+    ],
+  },
+  {
+    img: '/assets/svg/resume-screening-icon.svg',
+    title: 'Resume screening',
+    description:
+      'Utilize AI-driven resume screening to quickly identify top candidates by analyzing qualifications, experience, and skills against job requirements.',
+    detailData: [
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+    ],
+  },
+  {
+    img: '/assets/svg/work-flow-icon.svg',
+    title: 'Create A Work Flow',
+    description:
+      'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+    detailData: [
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+    ],
+  },
+  {
+    img: '/assets/svg/ats-icon.svg',
+    title: 'Integration With CRM/ATS',
+    description:
+      'Seamlessly connect with your existing CRM or Applicant Tracking System (ATS) to streamline candidate management and enhance data flow across platforms.',
+    detailData: [
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+      {
+        title: 'Main title one',
+        text: 'Our AI assists you in designing a personalized workflow, enabling automated decision-making based on candidate screening results.',
+      },
+    ],
   },
 ];
 
@@ -112,7 +200,7 @@ export default function Features({
           {featuresData.map((feature, idx) => (
             <motion.li
               variants={variants.fadeInUp}
-              className="max-w-[340px] self-stretch"
+              className="w-full max-w-[340px] self-stretch"
               key={idx}
             >
               <FeatureCard item={feature} />
@@ -125,10 +213,11 @@ export default function Features({
 }
 
 function FeatureCard({ item }: { item: IFeature }) {
-  const { img, title, description } = item;
+  const { img, title, description, detailData } = item;
+  console.log({ detailData });
   return (
-    <div className="bg-clrBlackPearl h-full rounded-xl p-6">
-      <div className="flex flex-col items-center gap-4">
+    <div className="bg-clrBlackPearl group relative h-full cursor-pointer rounded-xl p-6">
+      <div className="flex flex-col items-center gap-4 opacity-100 transition-all duration-500 ease-linear group-hover:opacity-0">
         <motion.div
           variants={variants.fadeInUp}
           className="h-[140px] w-full max-w-[240px]"
@@ -155,6 +244,39 @@ function FeatureCard({ item }: { item: IFeature }) {
           >
             {description}
           </motion.p>
+        </div>
+      </div>
+
+      <div className="bg-clrBlackPearl absolute inset-0 rounded-xl opacity-0 transition-all duration-500 ease-linear group-hover:opacity-100">
+        <div className="p-6">
+          {detailData.map(({ title, text }, idx) => (
+            <div key={idx}>
+              <motion.div
+                variants={variants.fadeInUp}
+                className="h-[0] w-full min-w-[240px]"
+              >
+                <Image
+                  className="size-full object-contain"
+                  height={100}
+                  width={100}
+                  src={img}
+                  alt={title}
+                />
+              </motion.div>
+              <motion.h3
+                variants={variants.fadeInUp}
+                className="font-nunito text-clrText mt-4 font-semibold uppercase"
+              >
+                {title}
+              </motion.h3>
+              <motion.p
+                variants={variants.fadeInUp}
+                className="font-nunito text-clrText/80 text-xs font-light"
+              >
+                {text}
+              </motion.p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
