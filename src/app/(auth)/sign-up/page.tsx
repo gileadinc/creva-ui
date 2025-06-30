abeca3a6012ab82db1bea5172c96d551f3546cea
render <142716427+render217@users.noreply.github.com>
2025-06-30 19:20:09 +0300

    fix: (landing-3) auth layout

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -5,10 +5,10 @@ import AuthBanner from '../_components/auth-banner';
 export default function SignUpPage() {
   return (
     <div
-      className={cn('dark:bg-clrWoodsmoke size-full min-h-screen px-[40px]')}
+      className={cn('dark:bg-clrWoodsmoke size-full min-h-[100vh] px-[40px]')}
     >
       <div className="grid h-full grid-cols-1 gap-4 lg:grid-cols-[1fr_1fr]">
-        <div className="z-10 mx-auto size-full w-full max-w-[500px] py-[2%] md:min-w-[400px]">
+        <div className="z-10 mx-auto grid min-h-[100vh] w-full max-w-[500px] place-items-center py-[2%] md:min-w-[400px]">
           <AuthForm formType="sign-up" />
         </div>
         <div className="relative hidden h-[calc(100vh-40px)] self-center lg:block">
